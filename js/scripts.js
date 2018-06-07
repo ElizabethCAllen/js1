1b7ef0390bba04b4808c0782652acec81ddfca0a
Elizabeth Allen <elizabethallen36@gmail.com>
2018-06-06 19:52:45 -0700

    takehome-day4

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,37 @@
+let todos= [];
+let todo1= [prompt("what do you want to do first?", "eat")];
+let todo2= [prompt("what do you want to do second?", "Eat")];
+let todo3= [prompt("what do you want to do third?", "Eat!")];
 
-const mov1= ["the Hobbit: An Unexpected Journey", 'Bilbo Baggins'];
-const mov2= ["Howl's Moving Castle", 'Sofie'];
-const mov3= ["The Hobbit: Desolation of Smaug", 'Bilbo Baggins'];
-const mov4= ["Troll Hunter", 'Hans'];
-const mov5= ["Raw", 'Justine'];
+todos.push(todo1, todo2, todo3);
 
-favoriteMovies.push(mov1, mov2, mov3, mov4, mov5);
-let titles=[];
-titles.push(favoriteMovies[0][0], favoriteMovies [1][0], favoriteMovies[2][0], favoriteMovies[3][0], favoriteMovies[4][0], favoriteMovies [5][0]);
+for (let i = 0; i < todos.length; i++) {
+
+  let timeline = prompt (`How many days will it take to get ${todos[i][0]} done?`, "2");
+
+  timeline = !isNaN(parseInt(timeline)) ?
+  parseInt(timeline) :2;
+  todos[i].unshift(timeline);
+}
+console.log(todos, 'after adding the time it will take');
+
+todos.sort();
+console.log(todos, 'after sort');
+
+let longest = todos[0];
+let j = 0;
+while(j < todos.length) {
+  if(todos[j][0] > longest[0]) longest = todos[j];
+  longest= todos[j][0] > longest[0] ? longest = todos[j] : longest = longest;
+  console.log(todos[j][1]);
+  j++;
+}
+longest.push('this');
+
+let z = 0;
+do {
+  if(todos[z][todos[z].length - 1] !== "this"){
+    todos[z].push("done");
+  }
+  z++;
+}while(z< todos.length);
