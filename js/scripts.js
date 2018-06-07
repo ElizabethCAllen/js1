adaa0cbed2cd421a808f241da0b94e712606f12d
Elizabeth Allen <elizabethallen36@gmail.com>
2018-06-06 20:43:23 -0700

    in class incomplete

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,14 @@
-let todos= [];
-let todo1= [prompt("what do you want to do first?", "eat")];
-let todo2= [prompt("what do you want to do second?", "Eat")];
-let todo3= [prompt("what do you want to do third?", "Eat!")];
-
-todos.push(todo1, todo2, todo3);
-
-for (let i = 0; i < todos.length; i++) {
-
-  let timeline = prompt (`How many days will it take to get ${todos[i][0]} done?`, "2");
-
-  timeline = !isNaN(parseInt(timeline)) ?
-  parseInt(timeline) :2;
-  todos[i].unshift(timeline);
-}
-console.log(todos, 'after adding the time it will take');
-
-todos.sort();
-console.log(todos, 'after sort');
-
-let longest = todos[0];
-let j = 0;
-while(j < todos.length) {
-  if(todos[j][0] > longest[0]) longest = todos[j];
-  longest= todos[j][0] > longest[0] ? longest = todos[j] : longest = longest;
-  console.log(todos[j][1]);
-  j++;
-}
-longest.push('this');
-
-let z = 0;
-do {
-  if(todos[z][todos[z].length - 1] !== "this"){
-    todos[z].push("done");
+// try it out #2
+function lifetimeCalculator(age, dailyUse) {
+  if(!isNaN(age) && !isNaN(dailyUse)){
+    const death = 80;
+    returnv(death - age) * (dailyUse *365);
+  }else {
+    userAge =prompt('how old are you?', '23');
+    userUse= prompt("How much do you use everyday?");
+    lifetimeCalculator(parseInt(userAge)), parseInt(userUse);
   }
-  z++;
-}while(z< todos.length);
+};
+let userAge =prompt('how old are you?', '23');
+let userUse= prompt("How much do you use everyday?", '1');
+let usuage= lifetimeCalculator(parseInt(userAge), parseInt(userUse));
